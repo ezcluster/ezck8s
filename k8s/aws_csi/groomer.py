6fbb769d2134f2216b78f2086cfb1ab8f1b083ef
SergeAlexandre <sa@broadsoftware.com>
2021-09-08 16:50:25 +0200

    Replace aws_plugin by k8s/aws_csi

--- k8s/aws_csi/groomer.py
+++ k8s/aws_csi/groomer.py
@@ -18,10 +18,9 @@
 from misc import setDefaultInMap, ERROR
 
 CLUSTER = "cluster"
-AWS_K8S = "aws_k8s"
 K8S = "k8s"
 AWS = "aws"
-EBS_CSI = "ebs_csi"
+AWS_CSI = "aws_csi"
 EBS_VOLUME_SCHEDULING = "volume_scheduling"
 EBS_VOLUME_SNAPSHOT = "volume_snapshot"
 EBS_VOLUME_RESIZING = "volume_resizing"
@@ -33,24 +32,20 @@ ROLE_BY_NAME = "roleByName"
 INSTANCE_ROLE_NAME = "instance_role_name"
 EBS_CSI_ENABLED = "ebs_csi_enabled"
 PERSISTENT_VOLUMES_ENABLED = "persistentVolumesEnabled"
-
+CONTROLLER_ON_CONTROL_PLANE="controller_on_control_plane"
 
 def groom(_plugin, model):
-    print("\n\nWARNING: The plugin aws_k8s is DEPRECATED. use k8s/aws_csi instead\n\n")
-    if K8S not in model[CLUSTER]:
-        ERROR("'aws_k8s' plugin could not be used if no 'k8s' plugin")
-    if AWS not in model[CLUSTER]:
-        ERROR("'aws_k8s' plugin could not be used if no 'aws' plugin")
-    setDefaultInMap(model[CLUSTER], AWS_K8S, {})
-    if EBS_CSI in model[CLUSTER][AWS_K8S]:
-        setDefaultInMap(model[CLUSTER][AWS_K8S][EBS_CSI], DISABLED, False)
-        setDefaultInMap(model[CLUSTER][AWS_K8S][EBS_CSI], EBS_VOLUME_SCHEDULING, True)
-        setDefaultInMap(model[CLUSTER][AWS_K8S][EBS_CSI], EBS_VOLUME_SNAPSHOT, False)
-        setDefaultInMap(model[CLUSTER][AWS_K8S][EBS_CSI], EBS_VOLUME_RESIZING, False)
-        setDefaultInMap(model[CLUSTER][AWS_K8S][EBS_CSI], EBS_CONTROLER_REPLICAS, 1)
-        setDefaultInMap(model[CLUSTER][AWS_K8S][EBS_CSI], EBS_PLUGIN_IMAGE_TAG, "latest")
-        if model[CLUSTER][AWS_K8S][EBS_CSI][DISABLED]:
-            del model[CLUSTER][AWS_K8S][AWS_K8S]
+    setDefaultInMap(model[CLUSTER], K8S, {})
+    if AWS_CSI in model[CLUSTER][K8S]:
+        setDefaultInMap(model[CLUSTER][K8S][AWS_CSI], DISABLED, False)
+        setDefaultInMap(model[CLUSTER][K8S][AWS_CSI], EBS_VOLUME_SCHEDULING, True)
+        setDefaultInMap(model[CLUSTER][K8S][AWS_CSI], EBS_VOLUME_SNAPSHOT, False)
+        setDefaultInMap(model[CLUSTER][K8S][AWS_CSI], EBS_VOLUME_RESIZING, False)
+        setDefaultInMap(model[CLUSTER][K8S][AWS_CSI], CONTROLLER_ON_CONTROL_PLANE, True)
+        setDefaultInMap(model[CLUSTER][K8S][AWS_CSI], EBS_CONTROLER_REPLICAS, 1)
+        setDefaultInMap(model[CLUSTER][K8S][AWS_CSI], EBS_PLUGIN_IMAGE_TAG, "latest")
+        if model[CLUSTER][K8S][AWS_CSI][DISABLED]:
+            del model[CLUSTER][K8S][AWS_CSI]
         else:
             model[DATA][K8S][PERSISTENT_VOLUMES_ENABLED] = True
             # Ensure we will have an instance role for all node allowed to use EBS CSI driver
